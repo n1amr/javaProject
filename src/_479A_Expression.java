4202968227fbfe57335a83af0f5fa1bdbdb54019
N1amr <n1amr1@gmail.com>
2014-11-07 18:46:54 +0200

    javaProject

---
+++ src/_479A_Expression.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class _479A_Expression {
+    public static void main(String[] args) throws IOException {
+	Scanner in = new Scanner(new FileInputStream(new File("input.txt")));
+	// Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int[] a = new int[n];
+	for (int i = 0; i < n; i++) {
+	    a[i] = in.nextInt();
+	    ;
+	}
+    }
+}
