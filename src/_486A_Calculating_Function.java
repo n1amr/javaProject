c44010ddc95754a7d407bedc93fbc857805bc432
N1amr <n1amr1@gmail.com>
2015-02-15 23:39:55 +0200

    _492A_Vanya_and_Cubes

--- src/_486A_Calculating_Function.java
+++ src/_486A_Calculating_Function.java
@@ -1,19 +1,8 @@
-import java.io.*;
 import java.util.*;
 
 public class _486A_Calculating_Function {
-    public static Scanner in = new Scanner(new BufferedReader(
-	    new InputStreamReader(System.in)));
-    public static PrintWriter out = new PrintWriter(new BufferedWriter(
-	    new OutputStreamWriter(System.out)));
-
     public static void main(String[] args) {
-	int n = in.nextInt();
-	int[] a = new int[n];
-	for (int i = 0; i < n; i++) {
-	    a[i] = in.nextInt();
-	}
-	out.println(Arrays.toString(a));
-	out.close();
+	long n = (new Scanner((System.in))).nextLong();
+	System.out.println(((n + 1) / 2) * (n % 2 == 1 ? -1 : 1));
     }
 }
---
+++ src/_492A_Vanya_and_Cubes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class _492A_Vanya_and_Cubes {
+    public static void main(String[] args) {
+	int n = (new Scanner(System.in)).nextInt();
+	int l = 0;
+	while (n >= 0) {
+	    l++;
+	    n -= l * (l + 1) / 2;
+	}
+	System.out.println(l - 1);
+    }
+}
