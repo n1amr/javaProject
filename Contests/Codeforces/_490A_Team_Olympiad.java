0201cc3f8292a6efa0fc7d94d75beb1c53d3bd64
N1amr <n1amr1@gmail.com>
2015-02-24 23:19:11 +0200

    496A - Minimum Difficulty

---
+++ Contests/Codeforces/_490A_Team_Olympiad.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class _490A_Team_Olympiad {
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	ArrayList[] arr = new ArrayList[3];
+	int[] c = new int[3];
+	for (int i = 0; i < 3; i++)
+	    arr[i] = new ArrayList();
+	int min = 9999;
+	for (int i = 0; i < n; i++) {
+	    int x = in.nextInt();
+	    c[x - 1]++;
+	    arr[x - 1].add(i + 1);
+	}
+	for (int i = 0; i < 3; i++)
+	    min = Math.min(min, c[i]);
+
+	System.out.println(min);
+	for (int i = 0; i < min; i++)
+	    System.out.println(arr[0].get(i) + " " + arr[1].get(i) + " "
+		    + arr[2].get(i));
+    }
+}
---
+++ Contests/Codeforces/_496A_Minimum_Difficulty.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class _496A_Minimum_Difficulty {
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int[] diff = new int[n - 1];
+	int n1 = in.nextInt(), n2 = 0;
+	for (int i = 0; i < n - 1; i++) {
+	    n2 = in.nextInt();
+	    diff[i] = Math.abs(n1 - n2);
+	    n1 = n2;
+	}
+	int min = diff[0] + diff[1];
+	int minIndex = 0;
+	for (int i = 0; i < diff.length - 1; i++) {
+	    int v = diff[i] + diff[i + 1];
+	    if (min > v) {
+		min = v;
+		minIndex = i;
+	    }
+	}
+	diff[minIndex] = min;
+	diff[minIndex + 1] = 0;
+	int max = diff[0];
+	for (int i = 0; i < diff.length; i++)
+	    max = Math.max(max, diff[i]);
+	System.out.println(max);
+    }
+}
