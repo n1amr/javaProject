f44e6e0513926b16edb8662ed83db713556c8fa8
N1amr <n1amr1@gmail.com>
2015-05-02 21:19:32 +0200

    Round 1B

--- Contests/Codejam/2015/Round 1B/CopyOfA.java
+++ Contests/Codejam/2015/Round 1B/CopyOfA.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class C {
+public class CopyOfA {
     public static Scanner in;
     public static PrintWriter out;
 
     public static void main(String[] args) throws FileNotFoundException {
 	in = new Scanner(new BufferedInputStream(new FileInputStream(new File(
-		"input.txt"))));
+		"A-small-attempt1.in"))));
 	out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(
 		new File("output.txt"))));
 	int nCases = in.nextInt();
@@ -21,6 +21,30 @@ public class C {
     }
 
     static void solve() {
-	out.println(in.nextLine());
+	long N = in.nextLong();
+	long r = f(1, 0, N);
+	System.out.println(r);
+	out.println(r);
+    }
+
+    static long f(long c, long r, long N) {
+	if (c > N)
+	    return 1;
+	if (c == N)
+	    return 1;
+
+	long r1 = f(c + 1, r, N);
+	if (flip(c) <= c + 1) {
+	    return 1 + r1;
+	}
+	long r2 = f(flip(c), r, N);
+
+	return 1 + Math.min(r1, r2);
+    }
+
+    static long flip(long n) {
+	String s = Long.toString(n);
+	String s2 = (new StringBuffer(s)).reverse().toString();
+	return Long.valueOf(s2);
     }
 }
---
+++ Contests/Codejam/2015/Round 1B/CopyOfB.java
@@ -0,0 +1,119 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class CopyOfB {
+
+    public static void main(String[] args) throws IOException {
+	CopyOfB main = new CopyOfB();
+	main.read();
+    }
+
+    /* IO */
+    private StringBuilder ans;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private StringTokenizer tok;
+
+    /* fields */
+    private int R;
+    private int C;
+    private int n;
+    private boolean[][] g;
+    private int dx[] = new int[] { 1, -1, 0, 0 };
+    private int dy[] = new int[] { 0, 0, 1, -1 };
+
+    private void read() throws IOException {
+	// streams
+	String inputFileName = "B-small-practice(1).in";
+	String outputFileName = "output.txt";
+	in = new BufferedReader(new FileReader(inputFileName));
+	out = new BufferedWriter(new FileWriter(outputFileName));
+	ans = new StringBuilder();
+
+	// read cases
+	int nCases = Integer.parseInt(in.readLine());
+	for (int cas = 1; cas <= nCases; cas++) {
+	    System.out.println("case: " + cas);
+	    tok = new StringTokenizer(in.readLine());
+	    R = Integer.parseInt(tok.nextToken());
+	    C = Integer.parseInt(tok.nextToken());
+	    n = Integer.parseInt(tok.nextToken());
+
+	    // solve
+	    ans.append("Case #" + cas + ": " + solve() + "\n");
+	}
+
+	// output
+	System.out.print(ans.toString());
+	out.write(ans.toString());
+	out.flush();
+	out.close();
+
+    }
+
+    private int solve() {
+	// try all bitmasks
+	int min = Integer.MAX_VALUE;
+	for (int mask = 0; mask < (1 << (R * C)); mask++) {
+	    // make bit mask
+	    g = new boolean[R][C];
+	    int count = 0;
+	    for (int i = 0; i < R * C; i++) {
+		int r = i / C;
+		int c = i % C;
+		if (((mask & (1 << i)) == 0))
+		    g[r][c] = false;
+		else {
+		    g[r][c] = true;
+		    count++;
+		}
+	    }
+	    if (count != n)
+		continue;
+
+	    // count unhappiness
+	    int unhap = countUnHappiness();
+	    min = Math.min(min, unhap);
+	}
+
+	return min;
+    }
+
+    private int countUnHappiness() {
+	int count = 0;
+	for (int r = 0; r < R; r++)
+	    for (int c = 0; c < C; c++)
+		if (g[r][c])
+		    for (int i = 0; i < 4; i++) {
+			int r2 = r + dx[i];
+			int c2 = c + dy[i];
+			if (valid(r2, c2))
+			    if (g[r2][c2])
+				count++;
+
+		    }
+	return count / 2;
+    }
+
+    private boolean valid(int r, int c) {
+	return r >= 0 && r < R && c >= 0 && c < C;
+    }
+
+    private void printG() {
+	for (int i = 0; i < R; i++) {
+	    for (int j = 0; j < C; j++)
+		if (g[i][j])
+		    System.out.print("1 ");
+		else
+		    System.out.print("0 ");
+	    System.out.println();
+	}
+	System.out.println();
+
+    }
+
+}
