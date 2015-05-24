7ad2bbe05063135acd0d7a66b0cbbfe791bee915
N1amr <n1amr1@gmail.com>
2015-05-24 21:33:14 +0200

    Store matrix

--- src/Console.java
+++ src/Console.java
@@ -9,14 +9,24 @@ public class Console {
 
     private static void run(String s) {
 	int i = 0;
+	String name = "";
+	String value;
 	if ((i = s.indexOf('=')) != -1) {
-	    String name = s.substring(0, i).trim();
-	    String value = s.substring(i + 1).trim();
-	    System.err.println(name);
-	    System.err.println(value);
-	    Mat X = new Mat(value);
-	    X.setName(name);
-	    System.out.println(X);
+	    name = s.substring(0, i).trim();
+	    value = evaluate(s.substring(i + 1).trim());
+
+	} else {
+	    value = evaluate(s.trim());
 	}
+	Mat X = new Mat(value);
+	if (name.length() == 0)
+	    name = "ans";
+	X.setName(name);
+	System.out.println(X);
+
+    }
+
+    private static String evaluate(String s) {
+	return s;
     }
 }
