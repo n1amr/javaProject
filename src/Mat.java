ae88a45a7a56fe3dc270dc07bffb16df8cd51f14
Amr Alaa <n1amr1@gmail.com>
2015-06-28 01:52:26 +0200

    Matrix fixes

--- src/Mat.java
+++ src/Mat.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class Mat {
     private float[][] data;
-    private int n, m;
-    private String name;
+    private int numberOfRows, numberOfColumns;
+    private String name = null;
 
     public String getName() {
 	return name;
@@ -13,39 +11,57 @@ public class Mat {
 	this.name = name;
     }
 
-    public Mat(int n, int m) {
-	data = new float[n][m];
-	this.n = n;
-	this.m = m;
+    /***
+     * Create empty matrix with specified dimensions
+     * 
+     * @param rows
+     *            number of rows
+     * @param cols
+     *            number of columns
+     * ***/
+    public Mat(int rows, int cols) {
+	data = new float[rows][cols];
+	this.numberOfRows = rows;
+	this.numberOfColumns = cols;
     }
 
+    /*** Create a matrix from a string ***/
     public Mat(String s) {
-	Mat.copyMat(this, Mat.valueOf(s));
+	Mat.copyMat(this, Mat.fromString(s));
     }
 
+    /** Creates a new copy of passed matrix **/
     public Mat duplicate(Mat src) {
-	Mat r = new Mat(src.n, src.m);
+	Mat r = new Mat(src.getNumberOfRows(), src.getNumberOfColumns());
 	Mat.copyMat(r, src);
 	return r;
     }
 
-    private static void copyMat(Mat dst, Mat src) {
-	int n = dst.n = src.n;
-	int m = dst.m = src.m;
-	dst.data = new float[n][m];
+    private static void copyMat(Mat destination, Mat source) {
+	int n = source.numberOfRows;
+	int m = source.numberOfColumns;
+
+	destination = new Mat(n, m);
+
 	for (int i = 0; i < n; i++)
 	    for (int j = 0; j < m; j++)
-		dst.data[i][j] = src.data[i][j];
+		destination.setElement(i, j, source.getElement(i, j));
+    }
+
+    public Mat(float sigleElement) {
+	numberOfRows = numberOfColumns = 1;
+	data = new float[1][1];
+	setElement(0, 0, sigleElement);
     }
 
     public Mat() {
-	new Mat(1, 1);
+	new Mat(0);
     }
 
-    public Mat(float k, int n, int m) {
-	data = new float[n][m];
-	this.n = n;
-	this.m = m;
+    public Mat(float k, int rows, int cols) {
+	data = new float[rows][cols];
+	this.numberOfRows = rows;
+	this.numberOfColumns = cols;
 
 	this.fill(k);
     }
@@ -55,35 +71,32 @@ public class Mat {
     }
 
     public void fill(float k) {
-	Mat r = new Mat(n, m);
-	for (int i = 0; i < n; i++) {
-	    for (int j = 0; j < m; j++) {
-		data[i][j] = k;
+	for (int i = 0; i < numberOfRows; i++) {
+	    for (int j = 0; j < numberOfColumns; j++) {
+		setElement(i, j, k);
 	    }
 	}
-	// return r;
     }
 
-    public int getN() {
-	return n;
+    public int getNumberOfRows() {
+	return numberOfRows;
     }
 
-    public int getM() {
-	return m;
+    public int getNumberOfColumns() {
+	return numberOfColumns;
     }
 
     @Override
     public String toString() {
 	StringBuilder s = new StringBuilder();
-	if (name != null) {
-	    s.append(name + " = \n");
-	}
+	// if (name != null) {
+	// s.append(name + " = \n");
+	// }
 	s.append("[");
-	for (int i = 0; i < n; i++) {
-	    for (int j = 0; j < m; j++) {
+	for (int i = 0; i < numberOfRows; i++) {
+	    for (int j = 0; j < numberOfColumns; j++) {
 		s.append("\t");
-		s.append(data[i][j]);
-
+		s.append(getElement(i, j));
 	    }
 	    s.append("\n");
 	}
@@ -97,25 +110,34 @@ public class Mat {
     }
 
     public Mat transpose() {
-	Mat ans = new Mat(m, n);
+	int n = getNumberOfRows();
+	int m = getNumberOfColumns();
+	Mat mat = new Mat(n, m);
 	for (int i = 0; i < m; i++) {
 	    for (int j = 0; j < n; j++) {
-		ans.data[i][j] = data[j][i];
+		mat.setElement(i, j, getElement(j, i));
 	    }
 	}
-	return ans;
+	return mat;
     }
+
+    private static final int OP_ADD = 1;
+    private static final int OP_SUBTRACT = 2;
+    private static final int OP_MULTIPLY = 3;
+    private static final int OP_DIVIDE = 4;
 
     private static float op(float a, float b, int Op) {
 	switch (Op) {
-	    case 1:
+	    case OP_ADD:
 		return a + b;
-	    case 2:
+	    case OP_SUBTRACT:
 		return a - b;
-	    case 3:
+	    case OP_MULTIPLY:
 		return a * b;
-	    case 4:
+	    case OP_DIVIDE:
 		return a / b;
+	    default:
+		// throw new Exception("Not a supported operation: " + Op);
 	}
 	return 0;
     }
@@ -129,16 +151,18 @@ public class Mat {
     }
 
     private static Mat elementOp(Mat A, Mat B, int Op) {
-	int n = A.getN();
-	int m = A.getM();
-	if (n != B.getN() || m != B.getM()) {
+	int n = A.getNumberOfRows();
+	int m = A.getNumberOfColumns();
+	if (n != B.getNumberOfRows() || m != B.getNumberOfColumns()) {
+	    // throw new Exception("Dimensions mismatch", new Throwable());
 	    System.err.println("Dimension Error");
 	    return null;
 	}
 	Mat ans = new Mat(n, m);
 	for (int i = 0; i < n; i++) {
 	    for (int j = 0; j < m; j++) {
-		ans.data[i][j] = op(A.data[i][j], B.data[i][j], Op);
+		float val = op(A.getElement(i, j), B.getElement(i, j), Op);
+		ans.setElement(i, j, val);
 	    }
 	}
 	return ans;
@@ -148,63 +172,100 @@ public class Mat {
      * Arithmetic Operations
      */
     public static Mat add(Mat A, Mat B) {
-	return elementOp(A, B, 1);
+	return elementOp(A, B, OP_ADD);
     }
 
-    public static Mat minus(Mat A, Mat B) {
-	return elementOp(A, B, 2);
+    public static Mat subtract(Mat A, Mat B) {
+	return elementOp(A, B, OP_SUBTRACT);
     }
 
     public static Mat times(Mat A, Mat B) {
-	return elementOp(A, B, 3);
+	return elementOp(A, B, OP_MULTIPLY);
     }
 
     public static Mat divide(Mat A, Mat B) {
-	return elementOp(A, B, 4);
+	return elementOp(A, B, OP_DIVIDE);
     }
 
-    public Mat subMat(int n1, int n2, int m1, int m2) {
-	Mat ans = new Mat(n2 - n1, m2 - m1);
-	for (int i = n1; i < n2; i++) {
-	    for (int j = m1; j < m2; j++) {
-		ans.data[i - n1][j - m1] = data[i][j];
+    public Mat subMat(int fromRow, int toRow, int fromColumn, int toColumn) {
+	Mat ans = new Mat(toRow - fromRow, toColumn - fromColumn);
+	for (int i = fromRow; i < toRow; i++) {
+	    for (int j = fromColumn; j < toColumn; j++) {
+		ans.setElement(i - fromRow, j - fromColumn,
+			this.getElement(i, j));
 	    }
 	}
 	return ans;
     }
 
-    public Mat getRow(int r) {
-	return subMat(r, r + 1, 0, m);
+    public Mat getRow(int row) {
+	return subMat(row, row + 1, 0, numberOfColumns);
     }
 
-    public Mat getColumn(int c) {
-	return subMat(0, n, c, c + 1);
+    public float[] getRowAsArray(int row) {
+	return getRow(row).data[0];
     }
 
-    public float getElement(int r, int c) {
-	return data[r][c];
+    public Mat getColumn(int column) {
+	return subMat(0, numberOfRows, column, column + 1);
     }
 
-    public void setRow(int r, float[] rowData) {
-	data[r] = rowData;
+    public float[] getColumnAsArray(int column) {
+	float[] arr = new float[this.getNumberOfRows()];
+	for (int i = 0; i < arr.length; i++) {
+	    arr[i] = this.getElement(i, column);
+	}
+	return arr;
     }
 
-    public void setRow(int r, Mat rowData) {
-	setRow(r, rowData.data[0]);
+    public float getElement(int row, int column) {
+	return data[row][column];
     }
 
-    public void setColumn(int c, float[] colData) {
-	for (int i = 0; i < n; i++) {
-	    data[i][c] = colData[i];
+    public void setRow(int row, float[] rowData) {
+	data[row] = rowData;
+    }
+
+    public void setRow(int row, Mat rowData) {
+	if (rowData.getNumberOfRows() != 0 && rowData.getNumberOfColumns() != 0) {
+	    if (rowData.getNumberOfRows() == 1
+		    && rowData.getNumberOfColumns() == this
+			    .getNumberOfColumns()) {
+		setRow(row, rowData.getRowAsArray(0));
+	    } else if (rowData.getNumberOfColumns() == 1
+		    && rowData.getNumberOfRows() == this.getNumberOfRows()) {
+		setRow(row, rowData.getColumnAsArray(0));
+	    }
+	} else {
+	    // throw new Exception("Dimensions error", new Throwable());
 	}
     }
 
-    public void setColumn(int c, Mat colData) {
-	setColumn(c, colData.data[0]);
+    public void setColumn(int column, float[] colData) {
+	for (int i = 0; i < numberOfRows; i++) {
+	    this.setElement(i, column, colData[i]);
+	}
     }
 
-    public void setElement(int r, int c, float x) {
-	data[r][c] = x;
+    public void setColumn(int column, Mat colData) {
+	if (colData.getNumberOfRows() != 0 && colData.getNumberOfColumns() != 0) {
+	    if (colData.getNumberOfRows() == 1
+		    && colData.getNumberOfColumns() == this
+			    .getNumberOfColumns()) {
+		setColumn(column, colData.getRowAsArray(0));
+	    } else if (colData.getNumberOfColumns() == 1
+		    && colData.getNumberOfRows() == this.getNumberOfRows()) {
+		setColumn(column, colData.getColumnAsArray(0));
+	    }
+	} else {
+	    // throw new Exception("Dimensions error", new Throwable());
+	}
+
+	setColumn(column, colData.data[0]);
+    }
+
+    public void setElement(int row, int column, float val) {
+	data[row][column] = val;
     }
 
     public static Mat inv(Mat A) {
@@ -212,77 +273,97 @@ public class Mat {
     }
 
     public Mat inv() {
-	if (n != m) {
+	if (numberOfRows != numberOfColumns) {
 	    System.err.println("Dimension Error");
 	    return null;
 	}
 	float s = 0; // /////////////////////////////////////////
-	Mat ans = new Mat(n, n);
-	int m = 2 * n;
-	Mat S = new Mat(n, m);
+
+	Mat ans = new Mat(getNumberOfRows(), getNumberOfRows());
+
+	int n = getNumberOfRows();
+	int m = getNumberOfColumns();
+	int m2 = 2 * getNumberOfColumns();
+
+	Mat S = new Mat(n, m2);
+
+	// Initialize
 	for (int i = 0; i < n; i++) {
-	    S.setElement(i, i + n, 1);
-	    for (int j = 0; j < n; j++) {
+	    S.setElement(i, i + m, 1);
+	    for (int j = 0; j < m; j++) {
 		S.setElement(i, j, this.getElement(i, j));
 	    }
 	}
+
 	for (int i = 0; i < n; i++) {
 	    float pivot = S.getElement(i, i);
-	    S.setRow(i, divide(S.getRow(i), new Mat(pivot, 1, m)));
+
+	    Mat newPivotRow = divide(S.getRow(i), new Mat(pivot, 1, m2));
+	    S.setRow(i, newPivotRow);
 
 	    for (int j = 0; j < n; j++) {
 		if (i == j)
-		    continue;
-		S.setRow(j, divide(S.getRow(j), new Mat(S.data[j][i], 1, m)));
-		S.setRow(j, minus(S.getRow(j), S.getRow(i)));
+		    continue;// was set before
+		Mat normalizedRow = divide(S.getRow(j),
+			new Mat(S.getElement(j, i), 1, m2));
+		S.setRow(j, normalizedRow);
+		S.setRow(j, subtract(S.getRow(j), S.getRow(i)));
 	    }
 	}
-	System.out.println(S);
+
+	// Normalize
 	for (int i = 0; i < n; i++) {
-	    S.setRow(i, divide(S.getRow(i), new Mat(S.data[i][i], 1, m)));
+	    S.setRow(i, divide(S.getRow(i), new Mat(S.data[i][i], 1, m2)));
 	}
-	ans = S.subMat(0, n, n, 2 * n);
+
+	ans = S.subMat(0, n, m, m2);
 	return ans;
     }
 
     public static Mat sumRows(Mat A) {
-	Mat ans = new Mat(1, A.m);
-	for (int j = 0; j < A.m; j++) {
-	    float sum = 0;
-	    for (int i = 0; i < A.n; i++) {
-		sum += A.data[i][j];
-	    }
-	    ans.data[0][j] = sum;
-	}
-	return ans;
+	return A.sumRows();
     }
 
     public Mat sumRows() {
-	return sumRows(this);
-    }
+	int n = this.getNumberOfRows();
+	int m = this.getNumberOfColumns();
+	Mat ans = new Mat(1, m);
 
-    public Mat sumCols(Mat A) {
-	Mat ans = new Mat(A.n, 1);
-	for (int i = 0; i < A.n; i++) {
-	    float sum = 0;
-	    for (int j = 0; j < A.m; j++) {
-		sum += A.data[i][j];
+	for (int j = 0; j < m; j++) {
+	    float columnSum = 0;
+	    for (int i = 0; i < n; i++) {
+		columnSum += this.getElement(i, j);
 	    }
-	    ans.data[i][0] = sum;
+	    ans.setElement(0, j, columnSum);
 	}
 	return ans;
     }
 
-    public Mat sumCols() {
-	return sumCols(this);
+    public static Mat sumCols(Mat A) {
+	return A.sumCols();
     }
 
-    public float sum(Mat A) {
-	return sumRows(sumCols(A)).data[0][0];
+    public Mat sumCols() {
+	int n = this.getNumberOfRows();
+	int m = this.getNumberOfColumns();
+	Mat ans = new Mat(n, 1);
+
+	for (int i = 0; i < n; i++) {
+	    float rowSum = 0;
+	    for (int j = 0; j < m; j++) {
+		rowSum += this.getElement(i, j);
+	    }
+	    ans.setElement(i, 0, rowSum);
+	}
+	return ans;
     }
 
     public float sum() {
-	return sum(this);
+	return sumRows().sumCols().getElement(0, 0);
+    }
+
+    public float sum(Mat A) {
+	return A.sum();
     }
 
     public static Mat multiply(Mat A, Mat B) {
@@ -290,35 +371,39 @@ public class Mat {
     }
 
     public Mat multiply(Mat A) {
-	if (this.m != A.n) {
+	// [n][m] * [m][p] = [n][p]
+	int n = this.getNumberOfRows();
+	int m = this.getNumberOfColumns();
+	int p = A.getNumberOfColumns();
+
+	if (m != A.numberOfRows) {
 	    System.err.println("Dimension Error");
 	    return null;
 	}
-	Mat ans = new Mat(this.n, A.m);
-	for (int i = 0; i < this.n; i++) {
-	    for (int j = 0; j < A.m; j++) {
-		ans.data[i][j] = sum(Mat.times(this.getRow(i), A.getColumn(j)
-			.transpose()));
+
+	Mat ans = new Mat(n, p);
+
+	for (int i = 0; i < n; i++) {
+	    for (int j = 0; j < p; j++) {
+		ans.setElement(i, j, sum(Mat.times(this.getRow(i),
+			A.getColumn(j).transpose())));
 	    }
 	}
 	return ans;
     }
 
-    public static float det(Mat A) {
-	return A.det();
-    }
-
     public float det() {
 	float sum = 0;
 	float sign;
-	if (n == 1)
+	if (numberOfRows == 1)
 	    return data[0][0];
 
-	for (int i = 0; i < n; i++) {
-	    Mat smaller = new Mat(n - 1, m - 1);
+	for (int i = 0; i < numberOfRows; i++) {
+	    Mat smaller = new Mat(numberOfRows - 1, numberOfColumns - 1);
 
-	    for (int a = 1; a < n; a++) {
-		for (int b = 0; b < n; b++) {
+	    // Copy determinant to smaller except current row & column
+	    for (int a = 1; a < numberOfRows; a++) {
+		for (int b = 0; b < numberOfRows; b++) {
 		    if (b < i)
 			smaller.data[a - 1][b] = data[a][b];
 		    else if (b > i)
@@ -335,40 +420,52 @@ public class Mat {
 	return sum;
     }
 
+    public static float det(Mat A) {
+	return A.det();
+    }
+
     public static void main(String[] args) {
 
-	String s = "[1,  2.123,3;  \n" + "4,5,6;7,8,9]";
-	Mat X = Mat.valueOf(s);
+	String s = "[2,1 ; 7,4]";
+	Mat X = Mat.fromString(s);
+	// X = X.transpose();
 	X.name = "Test Name";
 	System.out.println(X);
 	// System.out.println(det(Y));
 
-	Scanner in = new Scanner(System.in);
+	// Scanner in = new Scanner(System.in);
 
 	// while (in.hasNextLine())
-	Console.run("a=[1 2 3]");
-	Console.run("b=[2;2;2]");
-	Console.run("a*b");
+	// Console.run("a=[1 2 3]");
+	// Console.run("b=[2;2;2]");
+	// Console.run("a*b");
     }
 
     private static boolean isPartOfNumber(char c) {
 	return (Character.isDigit(c) || c == '.' || c == 'i' || c == 'j');
     }
 
-    private static Mat valueOf(String s) {
+    private static Mat fromString(String s) {
+
+	// Determine number of rows
 	int rows = 1;
 	for (int i = 0; i < s.length(); i++) {
 	    if (s.charAt(i) == ';')
 		rows++;
 	}
+
+	// Determine number of columns
 	int cols = 0;
 	for (int i = 0; i < s.length(); i++) {
-	    if (isPartOfNumber(s.charAt(i))) {
+	    char c = s.charAt(i);
+	    if (isPartOfNumber(c)) {
 		cols++;
-		while (isPartOfNumber(s.charAt(i)))
+		while (i < s.length() && isPartOfNumber(c) && c != ';') {
 		    i++;
+		    c = s.charAt(i);
+		}
 	    }
-	    if (s.charAt(i) == ';')
+	    if (c == ';')
 		break;
 	}
 
@@ -386,12 +483,17 @@ public class Mat {
 	    int num_start = i;
 	    while (i < s.length() && isPartOfNumber(s.charAt(i)))
 		i++;
-	    int num_end = i;
+	    int num_end = i; // Exclusive
 
 	    if (i < s.length()) {
 		String num = s.substring(num_start, num_end).trim();
 		float number = Float.valueOf(num);
-		row[c++] = number;
+		row[c] = number;
+		c++;
+		if (c == row.length) {
+		    while (i < s.length() && s.charAt(i) != ';')
+			i++;
+		}
 	    }
 	    if (i < s.length() && (s.charAt(i) == ';' || s.charAt(i) == ']')) {
 		X.setRow(r, row);
