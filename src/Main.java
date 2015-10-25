782c37f4d3a58eb6983fbea8be3f73a4ed9f7a73
Amr Alaa <n1amr1@gmail.com>
2015-10-25 15:56:14 +0200

    Clean up

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+	System.out.printf("%8s", "hello");
+	System.out.println(System.in.available());
+
+	Scanner scanner = new Scanner(System.in);
+    }
+}
