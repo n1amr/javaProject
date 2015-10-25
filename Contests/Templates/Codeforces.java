2bf5c8d0ad92a7c7e7fb014f92069dce060209b1
Amr Alaa <n1amr1@gmail.com>
2015-10-25 14:59:35 +0200

    Codeforces Round 591

--- Contests/Templates/Codeforces.java
+++ Contests/Templates/Codeforces.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Codeforces {
-    public static Scanner in = new Scanner(
-            new BufferedReader(new InputStreamReader(System.in)));
-    public static PrintWriter out = new PrintWriter(
-            new BufferedWriter(new OutputStreamWriter(System.out)));
+    public static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    public static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
     public static void main(String[] args) {
-        int n = in.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = in.nextInt();
-        }
-        out.println(Arrays.toString(a));
-        out.close();
+	int n = in.nextInt();
+	int[] a = new int[n];
+	for (int i = 0; i < n; i++) {
+	    a[i] = in.nextInt();
+	}
+	out.println(Arrays.toString(a));
+	out.close();
     }
 }
