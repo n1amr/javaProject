dc79a5ea4e9bd88550b42d6e48c42921510a1119
Amr Alaa <n1amr1@gmail.com>
2016-06-08 09:45:56 +0200

    617A Elephant

---
+++ Contests/Codeforces/_617_A_Elephant.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class _617_A_Elephant {
+	public static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+	public static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static void main(String[] args) {
+		int x = in.nextInt();
+		out.println((x - 1) / 5 + 1);
+		out.close();
+	}
+}
