381874db143aef9a99e64610624f656c31a863c5
Amr Alaa <n1amr1@gmail.com>
2016-08-23 22:53:27 +0200

    solve from HackerRank

---
+++ Contests/Hackerrank/CookieParty.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CookieParty {
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		int m = in.nextInt();
+
+		System.out.println((n - m%n)%n);
+	}
+}
